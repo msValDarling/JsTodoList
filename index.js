6c08956e9bc19fc5e5a5998d1a7faba6d4ddb6be
Mauricio <17030487@itcelaya.edu.mx>
2021-11-22 18:19:19 -0600

    tercer commit

--- index.js
+++ index.js
@@ -4,32 +4,51 @@ document.addEventListener('DOMContentLoaded', function(){
     const table = document.getElementById('table');
     const alert = document.getElementById('alert');
     const btn = document.getElementById('add');
-   
-    btn.onclick = addTodo;
-    
-    function addTodo(){
-       if(title.value === '' || description.value === '') {
-          alert.classList.remove('d-none');
-          alert.innerText = 'Title and description are required'
-       } 
-       alert.classList.add('d-none');
-       const row = table.insertRow();
-       row.innerHTML = `
-       <td>${title.value}</td>
-       <td>${description.value}</td>
-       <td class="text-center">
-       <input type="checkbox">
-     </td>
-     <td class="text-right">
-       <button class="btn btn-primary mb-1">
-         <i class="fa fa-pencil"></i>
-       </button>
-     </td>
-       `;
-       const removeBtn = document.createElement('button');
-       removeBtn.classList.add('btn', 'btn-danger');
-       removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
-       console.log(row.children[3]);
+    let id = 1;
+
+    /*btn.onclick = function(){
+        console.log('Title:', title.value);
+        console.log('Description:', description.value);
+    }*/
+
+    function removeTodo(id){
+        document.getElementById(id).remove();
     }
     
+    function addTodo(){
+        if(title.value === '' || description.value === ''){
+            alert.classList.remove('d-none');
+            alert.innerText = 'Title and description are required';
+            return;
+        }
+        alert.classList.add('d-none');
+        const row = table.insertRow();
+        row.setAttribute('id', id++);
+        row.innerHTML = `
+            <td>${title.value}</td>
+            <td>${description.value}</td>
+            <td class="text-center">
+                <input type="checkbox">
+            </td>
+            <td class="text-right">
+              <button class="btn btn-primary mb-1">
+                <i class="fa fa-pencil"></i>
+              </button>
+            </td>
+        `;
+        
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('btn','btn-danger','mb-1','ml-1');
+        removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+        removeBtn.onclick = function(e){
+            removeTodo(row.getAttribute('id'));
+        }
+        row.children[3].appendChild(removeBtn);
+    }
+    btn.onclick = addTodo;
 });
+
+
+/*btn.addEventListener('click',function(){
+    console.log('click');
+});*/
---
+++ js/model.js
@@ -0,0 +1,63 @@
+export default class Model{
+    constructor(){
+        this.view = null;
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+        if(!this.todos || this.todos.length < 1){
+            this.todos = [
+                {
+                    id: 0,
+                    title: 'Learn JS',
+                    description: 'Watch JS tutorials',
+                    completed: false
+                }
+            ]
+            this.currentId = 1;
+        }else{
+            this.currentId = this.todos[this.todos.length - 1].id + 1;
+        }
+    }
+
+    setView(view){
+        this.view = view;
+    }
+
+    save(){
+        localStorage.setItem('todos',JSON.stringify(this.todos));
+    }
+
+    getTodos(){
+       return this.todos.map((todo) => ({...todo}));
+    }
+
+    toggleCompleted(id){
+        const index = this.todos.findIndex((todo) => todo.id === id);
+        const todo = this.todos[index];
+        todo.completed = !todo.completed;
+        this.save();
+    }
+
+    editTodo(id, values){
+        const index = this.todos.findIndex((todo) => todo.id === id);
+        Object.assign(this.todos[index], values);
+        this.save();
+    }
+
+    addTodo(title,description){
+        const todo = {
+            id: this.currentId++,
+            title,
+            description,
+            completed: false,
+        }
+        this.todos.push(todo);
+        //return Object.assign({},todo);
+        this.save();
+        return{... todo};
+    }
+
+    removeTodo(id){
+        const index = this.todos.findIndex((todo) => todo.id === id);
+        this.todos.splice(index, 1);
+        this.save();
+    }
+}
